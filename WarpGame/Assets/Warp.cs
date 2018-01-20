0d38621d82af5e4cc892c31483897b63c5d3cad7
David Ly <d.ly@mail.utoronto.ca>
2018-01-20 13:51:39 -0500

    player reycast instant movement

---
+++ WarpGame/Assets/Warp.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+
+    public float warpVelocity = 10000;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            //GetComponent<Rigidbody>().velocity = ;
+            //GetComponent<Rigidbody>().velocity += this.transform.forward * warpVelocity;
+            Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+            Debug.DrawRay(transform.position, forward, Color.green);
+
+            if (Physics.Raycast(transform.position, forward, 10))
+            {
+                GetComponent<Rigidbody>().transform.position += this.transform.forward * warpVelocity;
+                print("There is something in front of the object!");
+            }
+        }
+        /*if (Input.GetButton("Fire1"))
+        {
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+            if (Physics.Raycast(transform.position, forward, 10))
+            {
+                GetComponent<Rigidbody>().velocity += this.transform.forward * warpVelocity;
+                print("There is something in front of the object!");
+            }
+        }*/
+
+    }
+
+    /*void FixedUpdate()
+    {
+        
+    }*/
+}
