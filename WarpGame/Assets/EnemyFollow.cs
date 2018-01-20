d2ad2be8f62606249603528031fcb4b0e7481b82
xyecs <runedesired2@gmail.com>
2018-01-20 13:50:09 -0500

    enemy

---
+++ WarpGame/Assets/EnemyFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+	public GameObject Player;
+	public GameObject Enemy;
+	public float EnemySpeed;
+
+	// Use this for initialization
+	void Start () {
+		EnemySpeed = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, EnemySpeed);
+	}
+}
